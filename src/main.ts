6b6150eb2db71d06446503d2dd98f11489dd3839
Vidit Singh Gautam <shanaaya.vidit.dev@gmail.com>
2025-02-15 16:48:08 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // You can specify allowed origins here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
