c4614539b7280238b630d03be13d004d1cf6d326
Vidit Singh Gautam <shanaaya.vidit.dev@gmail.com>
2025-02-17 01:05:45 +0530

    Bug Fixed

--- src/global/socket.gateway.ts
+++ src/global/socket.gateway.ts
@@ -36,7 +36,9 @@ export class SocketGateway {
     );
     this.server.on('connection', (client: Socket) => {
       console.log(`Client connected: ${client.id}`);
-      client.on('disconnect', () => {
+      client.on('disconnect', async() => {
+        let queue = await this.cacheManager.get('queue')
+        if(queue.)
         console.log(`Client disconnected: ${client.id}`);
       });
     });
@@ -68,6 +70,7 @@ export class SocketGateway {
     } else {
       const secondClientId = queue.shift();
       if (secondClientId) {
+        await this.cacheManager.set('queue', queue);
         // Emit that the second client has been found (you can emit an event if you want)
         const secondClient = this.server.sockets.sockets.get(secondClientId);
         if (secondClient) {
@@ -97,60 +100,62 @@ export class SocketGateway {
 
   async startQuestions(roomName: string) {
     console.log('eNTERed');
-    const questions = [
+    let questions: object[] = [
       {
         question: 'What is the square root of 144?',
         options: ['10', '12', '14', '16'],
-        correct: '12',
+        correct: 1,
       },
       {
         question: 'What is 15% of 200?',
         options: ['30', '40', '50', '60'],
-        correct: '30',
+        correct: 0,
       },
       {
         question: 'What is 25 times 16?',
         options: ['400', '375', '500', '425'],
-        correct: '400',
+        correct: 0,
       },
       {
         question: 'What is the result of 80 divided by 4?',
         options: ['16', '18', '20', '22'],
-        correct: '20',
+        correct: 2,
       },
       {
         question: 'If x = 3, what is the value of 4x + 5?',
         options: ['17', '15', '13', '11'],
-        correct: '17',
+        correct: 0,
       },
       {
         question: 'What is the value of 9^2?',
         options: ['81', '72', '90', '100'],
-        correct: '81',
+        correct: 0,
       },
       {
         question: 'What is the product of 7 and 13?',
         options: ['84', '78', '91', '98'],
-        correct: '91',
+        correct: 2,
       },
       {
         question:
           'If a train travels 60 miles in 1 hour, how far will it travel in 3.5 hours?',
         options: ['210 miles', '200 miles', '220 miles', '240 miles'],
-        correct: '210 miles',
+        correct: 0,
       },
       {
         question:
           'What is the area of a triangle with a base of 10 units and a height of 5 units?',
         options: ['25', '30', '40', '50'],
-        correct: '25',
+        correct: 0,
       },
       {
         question: 'What is the sum of 132 and 768?',
-        options: ['900', '950', '900', '850'],
-        correct: '900',
+        options: ['900', '950', '910', '850'],
+        correct: 0,
       },
     ];
+
+    questions = await this.getRandomizedQuestions(questions);
 
     let index = 0;
     let timeOut;
@@ -166,5 +171,19 @@ export class SocketGateway {
         index += 1;
       }
     }, 10000);
+  }
+
+  async getRandomizedQuestions(questions: object[]): Promise<object[]> {
+    const shuffledQuestions = [...questions];
+
+    for (let i = shuffledQuestions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledQuestions[i], shuffledQuestions[j]] = [
+        shuffledQuestions[j],
+        shuffledQuestions[i],
+      ];
+    }
+
+    return shuffledQuestions.slice(0, 4);
   }
 }
